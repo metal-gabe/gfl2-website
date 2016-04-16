b5369a2079e2e3adfe3b5195c27942497c7a8a20
A New Level Media <anewlevelmp@gmail.com>
2016-04-15 19:53:02 -0700

    setting up initial files, like the default.html placeholder

---
+++ scripts/php/base-url.php
@@ -0,0 +1,7 @@
+<?php
+	// Setting the PRODUCTION base URL for all pages on the site - Toggle comment to make active/inactive
+	define('BASE_URL', 'http://gflujan.com');
+
+	// Setting the STAGING base URL for all pages on the site - Toggle comment to make active/inactive
+	// define('BASE_URL', 'http://staging.gflujan.com/2-gflujan');
+?>
