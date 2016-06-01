40340f458d0bea4c48f0edaa64f0d351f061d444
Gabriel Lujan <contact@anewlevelmedia.com>
2016-05-31 23:46:57 -0700

    adding more foundation HTML and the starting pieces for the Projects carousel

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 <!-- ********************************************* HEAD: START ********************************************** -->
 <HEAD>
 
-    <meta charset="UTF-8">
-    <meta name="author" content="Gabriel F. Lujan">
-    <meta name="description" content="This is the portfolio site for all things pertaining to, and created by, Gabriel F. Lujan.">
-    <meta name="keywords" content="gabriel, floyd, lujan, portfolio, site, ux, ui, user, experience, interface, design, creative, audio, video, editing, post, production, sublime, text, 3, transmit, git, github, iterm, firefox, chrome, mozilla, developer, experiential, programming, html5, css3, html, css, javascript, jquery, bootstrap, modernizr, initializr, boilerplate, custom, built, website, websites, mockup, sketch, app, invision, vivaldi">
+	<meta charset="UTF-8">
+	<meta name="author" content="Gabriel F. Lujan">
+	<meta name="description" content="This is the portfolio site for all things pertaining to, and created by, Gabriel F. Lujan.">
+	<meta name="keywords" content="gabriel, floyd, lujan, portfolio, site, ux, ui, user, experience, interface, design, creative, audio, video, editing, post, production, sublime, text, 3, transmit, git, github, iterm, firefox, chrome, mozilla, developer, experiential, programming, html5, css3, html, css, javascript, jquery, bootstrap, modernizr, initializr, boilerplate, custom, built, website, websites, mockup, sketch, app, invision, vivaldi">
 
 	<title>Gabriel F. Lujan</title>
 
@@ -29,9 +29,44 @@
 
 	<!-- MAIN TRUNK OF Gabriel's Portfolio -->
 	<div id="CONTAINER">
-		<header></header>
-		<section id="content"></section>
-		<footer></footer>
+		<header>
+			<nav id="main">
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</nav>
+			<div id="hero"></div>
+		</header>
+		<section id="content">
+			<div id="case-study"></div>
+			<div id="projects">
+				<div id="left-scroll"><img src="left.png" /></div>
+				<div id="carousel-inner">
+					<ul id="carousel-ul">
+						<li><a href="#"><img src="item1.png" /></a></li>
+						<li><a href="#"><img src="item2.png" /></a></li>
+						<li><a href="#"><img src="item3.png" /></a></li>
+						<li><a href="#"><img src="item4.png" /></a></li>
+						<li><a href="#"><img src="item5.png" /></a></li>
+					</ul>
+				</div>
+				<div id="right-scroll"><img src="right.png" /></div>
+			</div>
+			<div id="skills"></div>
+		</section>
+		<footer>
+			<nav id="social-media">
+				<ul>
+					<li class="social-badge lnkd"></li>
+					<li class="social-badge beha"></li>
+					<li class="social-badge drib"></li>
+					<li class="social-badge twit"></li>
+				</ul>
+			</nav>
+		</footer>
 	</div>
 
 	<!-- VENDOR SCRIPTS -->
