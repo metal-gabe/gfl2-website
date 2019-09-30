1916ca20557c8478cd49def112e50e6ca3259014
metal_gabe <glujan@meredith.com>
2019-09-29 22:18:34 -0700

    update index & main.css syntax styling

--- index.php
+++ index.php
@@ -2,110 +2,108 @@
 <?php include('scripts/php/base-url.php'); ?>
 
 <!DOCTYPE html>
+
 <HTML lang="en">
-
 <!-- Pg. 01, Gabriel F. Lujan -->
-<!-- ********************************************* HEAD: START ********************************************** -->
-<HEAD>
+  <!-- ********************************************* HEAD: START ********************************************** -->
+  <HEAD>
 
-	<meta charset="UTF-8">
-	<meta name="author" content="Gabriel F. Lujan">
-	<meta name="description" content="This is the portfolio site for all things pertaining to, and created by, Gabriel F. Lujan.">
-	<meta name="keywords" content="gabriel, floyd, lujan, portfolio, site, ux, ui, user, experience, interface, design, creative, audio, video, editing, post, production, sublime, text, 3, transmit, git, github, iterm, firefox, chrome, mozilla, developer, experiential, programming, html5, css3, html, css, javascript, jquery, bootstrap, modernizr, initializr, boilerplate, custom, built, website, websites, mockup, sketch, app, invision, vivaldi">
+    <meta charset="UTF-8">
+    <meta name="author" content="Gabriel F. Lujan">
+    <meta name="description" content="This is the portfolio site for all things pertaining to, and created by, Gabriel F. Lujan.">
+    <meta name="keywords" content="gabriel, floyd, lujan, portfolio, site, ux, ui, user, experience, interface, design, creative, audio, video, editing, post, production, sublime, text, 3, transmit, git, github, iterm, firefox, chrome, mozilla, developer, experiential, programming, html5, css3, html, css, javascript, jquery, bootstrap, modernizr, initializr, boilerplate, custom, built, website, websites, mockup, sketch, app, invision, vivaldi">
 
-	<title>Gabriel F. Lujan - UX/UI Designer</title>
+    <title>Gabriel F. Lujan - UX/UI Designer</title>
 
-	<!-- VENDOR STYLES -->
-	<link rel="stylesheet" href="<?php echo BASE_URL; ?>/styles/css/bootstrap.min.css">
+    <!-- VENDOR STYLES -->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/styles/css/bootstrap.min.css">
 
-	<!-- CUSTOM STYLES -->
-	<link rel="stylesheet" href="<?php echo BASE_URL; ?>/styles/css/main.css">
+    <!-- CUSTOM STYLES -->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/styles/css/main.css">
 
-</HEAD>
-
-
-<!-- ********************************************* BODY: START ********************************************** -->
-<BODY>
-
-	<!-- MAIN TRUNK OF Gabriel's Portfolio -->
-	<header class="row">
-		<div id="intro">
-			<p id="gfl">Gabriel F. Lujan</p>
-			<p id="tagline">
-				<span class="quote">It's never too late to be who you might have been.</span> - Unknown
-			</p>
-		</div>
-		<div id="head">
-			<nav id="main">
-				<ul>
-					<a href="#" target="_blank"><li id="home" class="col-md-3 main-nav"></li></a>
-					<a href="#" target="_blank"><li id="about" class="col-md-3 main-nav"></li></a>
-					<a href="#" target="_blank"><li id="overview" class="col-md-3 main-nav"></li></a>
-					<a href="#" target="_blank"><li id="contact" class="col-md-3 main-nav"></li></a>
-				</ul>
-			</nav>
-			<div id="hero"></div>
-		</div>
-	</header>
-	<div id="CONTAINER" class="container">
-		<section id="content" class="row container-fluid">
-			<h1>Case Study</h1>
-			<div id="case-study" class="row"></div>
-			<h1>Projects</h1>
-			<div id="projects" class="row">
-				<div id="carousel" class="row">
-					<div id="left-scroll" class="col-md-1"></div>
-					<ul class="col-md-12">
-						<li id="proj1" class="col-md-4 project"></li>
-						<li id="proj2" class="col-md-4 project"></li>
-						<li id="proj3" class="col-md-4 project"></li>
-					</ul>
-					<div id="right-scroll" class="col-md-1"></div>
-				</div>
-			</div>
-			<h1>Skills</h1>
-			<div id="skills" class="row">
-				<ul class="col-md-4">
-					<li>Skill 1</li>
-					<li>Skill 2</li>
-					<li>Skill 3</li>
-				</ul>
-				<ul class="col-md-4">
-					<li>Skill 1</li>
-					<li>Skill 2</li>
-					<li>Skill 3</li>
-				</ul>
-				<ul class="col-md-4">
-					<li>Skill 1</li>
-					<li>Skill 2</li>
-					<li>Skill 3</li>
-				</ul>
-			</div>
-		</section>
-	</div>
-	<footer class="row">
-		<nav id="social-media" class="col-md-4 col-md-offset-8 col-sm-12 col-sm-offset-0">
-			<ul>
-				<a href="https://www.linkedin.com/in/gflujan/" target="_blank"><li id="lnkd" class="col-md-3 social-badge"></li></a>
-				<a href="https://www.behance.net/anewlevelmedia" target="_blank"><li id="bhnc" class="col-md-3 social-badge"></li></a>
-				<a href="https://dribbble.com/ANewLevelMedia" target="_blank"><li id="drib" class="col-md-3 social-badge"></li></a>
-				<a href="https://twitter.com/gabriel_lujan_" target="_blank"><li id="twit" class="col-md-3 social-badge"></li></a>
-			</ul>
-		</nav>
-		<div id="legal">
-			Gabriel F. Lujan &copy; <span id="copyright"></span><br>
-			All Rights Reserved
-		</div>
-	</footer>
-
-	<!-- VENDOR SCRIPTS -->
-	<script src="<?php echo BASE_URL; ?>/scripts/vendor/jquery-1.11.2.min.js"></script>
-	<script src="<?php echo BASE_URL; ?>/scripts/vendor/bootstrap.min.js"></script>
-
-	<!-- CUSTOM SCRIPTS -->
-	<script src="<?php echo BASE_URL; ?>/scripts/js/main.js"></script>
-
-</BODY>
+  </HEAD>
 
 
+  <!-- ********************************************* BODY: START ********************************************** -->
+  <BODY>
+
+    <!-- MAIN TRUNK OF Gabriel's Portfolio -->
+    <header class="row">
+      <div id="intro">
+        <p id="gfl">Gabriel F. Lujan</p>
+        <p id="tagline">
+          <span class="quote">It's never too late to be who you might have been.</span> - Unknown
+        </p>
+      </div>
+      <div id="head">
+        <nav id="main">
+          <ul>
+            <a href="#" target="_blank"><li id="home" class="col-md-3 main-nav"></li></a>
+            <a href="#" target="_blank"><li id="about" class="col-md-3 main-nav"></li></a>
+            <a href="#" target="_blank"><li id="overview" class="col-md-3 main-nav"></li></a>
+            <a href="#" target="_blank"><li id="contact" class="col-md-3 main-nav"></li></a>
+          </ul>
+        </nav>
+        <div id="hero"></div>
+      </div>
+    </header>
+    <div id="CONTAINER" class="container">
+      <section id="content" class="row container-fluid">
+        <h1>Case Study</h1>
+        <div id="case-study" class="row"></div>
+        <h1>Projects</h1>
+        <div id="projects" class="row">
+          <div id="carousel" class="row">
+            <div id="left-scroll" class="col-md-1"></div>
+            <ul class="col-md-12">
+              <li id="proj1" class="col-md-4 project"></li>
+              <li id="proj2" class="col-md-4 project"></li>
+              <li id="proj3" class="col-md-4 project"></li>
+            </ul>
+            <div id="right-scroll" class="col-md-1"></div>
+          </div>
+        </div>
+        <h1>Skills</h1>
+        <div id="skills" class="row">
+          <ul class="col-md-4">
+            <li>Skill 1</li>
+            <li>Skill 2</li>
+            <li>Skill 3</li>
+          </ul>
+          <ul class="col-md-4">
+            <li>Skill 1</li>
+            <li>Skill 2</li>
+            <li>Skill 3</li>
+          </ul>
+          <ul class="col-md-4">
+            <li>Skill 1</li>
+            <li>Skill 2</li>
+            <li>Skill 3</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+    <footer class="row">
+      <nav id="social-media" class="col-md-4 col-md-offset-8 col-sm-12 col-sm-offset-0">
+        <ul>
+          <a href="https://www.linkedin.com/in/gflujan/" target="_blank"><li id="lnkd" class="col-md-3 social-badge"></li></a>
+          <a href="https://www.behance.net/anewlevelmedia" target="_blank"><li id="bhnc" class="col-md-3 social-badge"></li></a>
+          <a href="https://dribbble.com/ANewLevelMedia" target="_blank"><li id="drib" class="col-md-3 social-badge"></li></a>
+          <a href="https://twitter.com/gabriel_lujan_" target="_blank"><li id="twit" class="col-md-3 social-badge"></li></a>
+        </ul>
+      </nav>
+      <div id="legal">
+        Gabriel F. Lujan &copy; <span id="copyright"></span><br>
+        All Rights Reserved
+      </div>
+    </footer>
+
+    <!-- VENDOR SCRIPTS -->
+    <script src="<?php echo BASE_URL; ?>/scripts/vendor/jquery-1.11.2.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>/scripts/vendor/bootstrap.min.js"></script>
+
+    <!-- CUSTOM SCRIPTS -->
+    <script src="<?php echo BASE_URL; ?>/scripts/js/main.js"></script>
+
+  </BODY>
 </HTML>
